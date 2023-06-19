0feca1aef0fd64a504b3773aa08e5e499dc95d45
Nameless <dev@name1ess.ga>
2023-06-19 23:05:15 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,11 +1,11 @@
 --[[
 
-Redwood Prison Script / 2019 - 2022
+Redwood Prison Script / 2019 - 2023
 
-Redwood Prison GUI Created By Nameless (Dev) & Jalos34 (Ex-Dev)
+Redwood Prison GUI Created By Nameless (Ex-Dev) & Jalos34 (Ex-Dev)
 
 Discord Link : https://discordapp.com/invite/ZX8segewN2 (Purchase premium whitelist here)
-TOS Link: https://github.com/Nameless9000/Redwood-GUI/blob/main/TOS.md (Please Read)
+TOS Link: https://github.com/checkm4te8/Redwood-GUI/blob/main/TOS.md (Please Read)
 
 Notice:
 Removing content from this paste with intentions of posting it elsewhere may result in a blacklist if we feel that it is necessary to take action.
@@ -19,4 +19,4 @@ _G.SetSecondaryColor("#ffffff")
 _G.GUICLICKID = "6706935653"
 _G.AimAssist = false
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Nameless9000/Redwood-GUI/main/Script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/checkm4te8/Redwood-GUI/main/Script.lua"))()
